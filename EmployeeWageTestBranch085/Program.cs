f0c239e43e4d1f0ac0f0c5cec04dbb3128c07d04
Pavithra <nspavi1991@gmail.com>
2021-11-29 11:50:16 +0530

    [Pavithra Modify Checking employee presence]

--- EmployeeWageTestBranch085/Program.cs
+++ EmployeeWageTestBranch085/Program.cs
@@ -10,7 +10,19 @@ namespace EmployeeWageTestBranch085
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 1;
             Console.WriteLine("Welcome to EmployeeWage computation");
+
+            Random random = new Random();
+            int empCheck= random.Next(0,2);
+            if(empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
